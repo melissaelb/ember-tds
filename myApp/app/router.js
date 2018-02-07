0b263f60fb34251591994d945e0493f1396f0b91
Melissa Elban <luffa@melissa.com>
2018-02-07 15:25:04 +0100

    Ajout ex1

--- myApp/app/router.js
+++ myApp/app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('ex1');
 });
 
 export default Router;
---
+++ myApp/app/routes/ex1.js
@@ -0,0 +1,42 @@
+import Route from '@ember/routing/route';
+import EmberObject, {computed} from '@ember/object';
+import { notEmpty } from '@ember/object/computed';
+
+const Note = EmberObject.extend({
+  alertVisible:notEmpty('info'),
+  size:computed('content',function(){
+    let content = this.get('content');
+    if(content)
+      this.set('info','Note modifiée');
+    let MAX = this.get('MAX');
+    return MAX-content.length;
+  }),
+  style:computed('size',function(){
+    let size=this.get('size');
+    let style='alert-info';
+    if(size<50)
+      style='alert-warning';
+    if(size<20)
+      style='alert-danger';
+    return style;
+  })
+});
+
+export default Route.extend({
+  model() {
+    return Note.create({
+      MAX:100,
+      content: ''
+    });
+  },
+  actions:{
+    save:function(model) {
+      model.set('info', `Note sauvegardée ${model.get('content')}`);
+    },
+    clear:function() {
+      let model=this.modelFor(this.routeName);
+      model.set('content','');
+      model.set('info','');
+    }
+  }
+});
